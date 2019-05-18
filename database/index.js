0f9172854fe86674096026110a82f3e73713b2dd
Kenny Szeto <kenny.szeto1@gmail.com>
2019-05-17 17:50:43 -0700

    implemented seeding

--- database/index.js
+++ database/index.js
@@ -1,32 +1,48 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/photoGalleryDB', { 
-  useNewUrlParser: true
-}); 
+/* eslint-disable array-callback-return */
+const mongoose = require('mongoose');
 
-let photoGallerySchema = mongoose.Schema({
-    listingId: Number,
-    imageUrl: String,
-    description: String
-  });
-
-let Photo = mongoose.model('Photo', photoGallerySchema);
-
-var test = new Photo({listingId: 123, imageUrl: 'path.test', description: 'testing database'});
-test.save(function (err) {
-  if (err) {
-    console.log(err);
-  }
+mongoose.connect('mongodb://localhost/photoGalleryDB', {
+  useNewUrlParser: true,
 });
+
+
+const photoGallerySchema = mongoose.Schema({
+  listingId: Number,
+  imageUrl: Array,
+  caption: Array,
+  verified: Boolean,
+});
+
+const Photo = mongoose.model('Photo', photoGallerySchema);
 
 const findAllPhotos = (cb) => {
   console.log('successfully reached database');
-  Photo.find(function (err, data) {
+  Photo.find((err, data) => {
     if (err) {
       cb(err);
     } else {
       cb(null, data);
     }
-  })
-}
+  });
+};
+
+const saveAllPhotos = (imageObj) => {
+  const newDocument = new Photo({
+    listingId: imageObj.listingId,
+    imageUrl: imageObj.imageUrls,
+    caption: imageObj.caption,
+    verified: imageObj.verified,
+  });
+
+  newDocument.save((err) => {
+    if (err) {
+      console.log('failed to save');
+    } else {
+      console.log('successfully saved');
+    }
+  });
+};
+
 
 module.exports.findAllPhotos = findAllPhotos;
+module.exports.saveAllPhotos = saveAllPhotos;
