693ca3803a6336ed3810144fbc3e3ac23f6ac9af
Kenny Szeto <kenny.szeto1@gmail.com>
2019-05-22 19:53:41 -0700

    testing progress

--- spec/app.test.js
+++ spec/app.test.js
@@ -1,7 +1,9 @@
 import React from 'react';
 import sinon from 'sinon';
-import { shallow } from 'enzyme';
+import { shallow, mount } from 'enzyme';
 import App from '../client/src/components/App.jsx';
+import ImageGrid from '../client/src/components/ImageGrid.jsx';
+
 
 describe('App Component Test Suite', () => {
   it('App component should be defined', () => {
@@ -10,10 +12,18 @@ describe('App Component Test Suite', () => {
     expect(wrapper.state().hasLoaded).toBe(true);
   });
 
-  it('simulates click events', () => {
-    const onButtonClick = sinon.spy();
-    const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
-    wrapper.find('button').simulate('click');
-    expect(onButtonClick).to.have.property('callCount', 1);
+  // it('simulates click events', () => {
+  //   const onButtonClick = sinon.spy();
+  //   const wrapper = shallow(<Foo onButtonClick={onButtonClick} />);
+  //   wrapper.find('button').simulate('click');
+  //   expect(onButtonClick).to.have.property('callCount', 1);
+  // });
+
+  it('Renders Image Grid component', () => {
+    const wrapper = shallow(<ImageGrid />);
+    expect()
+  //   const wrapper = shallow(<App />);
+  //   expect(App.prototype.componentDidMount).to.have.property('callCount', 1);
+  //   App.prototype.componentDidMount.restore();
   });
 });
